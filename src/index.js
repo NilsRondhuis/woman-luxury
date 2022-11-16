4f5e7499e944bc54452957211c003cd3ad5b5d98
NilsRondhuis <nilsrondhuis@ukr.net>
2022-11-16 23:32:29 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import "../node_modules/swiper/swiper-bundle.min.css";
+import "./sass/main.scss";
+import "./js/slider";
---
+++ src/js/slider.js
@@ -0,0 +1,9 @@
+import Swiper, { Pagination } from "swiper";
+Swiper.use([Pagination]);
+
+const reviews = new Swiper(".reviews-slider", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
